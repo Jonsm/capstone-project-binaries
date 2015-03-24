75732abf8a7d312f83881835269b652b9decb16c
samsloate1 <samsloate1@gmail.com>
2015-03-23 20:26:18 -0400

    Marching Cubes

---
+++ Standard Assets/Rain/RainAssets/scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.transform.position = new Vector3(Camera.main.gameObject.transform.position.x,
+		                                                 Camera.main.gameObject.transform.position.y+ 200,
+		                                                 Camera.main.gameObject.transform.position.z);
+
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.position = new Vector3(Camera.main.gameObject.transform.position.x,
+		                                                 Camera.main.gameObject.transform.position.y+ 200,
+		                                                 Camera.main.gameObject.transform.position.z);
+
+	}
+}
